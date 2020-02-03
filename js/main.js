379653b00ce2c0855599a4a517c176b3943ad354
Josiah Nieves <23145687+jtn7@users.noreply.github.com>
2020-02-03 18:13:45 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var carouselImgs = ['jar-sun', 'facing-up', 'underwater', 'water-hands']
+var carouselCounter = 0
+var carousel = null
+
+setInterval(function(){carousel_right()},5000)
+
+function carousel_left() {
+	if (carouselCounter == 0) {
+		carouselCounter = 3
+	} else {
+		--carouselCounter
+	}
+	document.getElementById("carousel").style.backgroundImage = 'url("../img/' + carouselImgs[carouselCounter] + '.jpg")'
+	// carousel.style.backgroundColor = 'black'
+}
+
+function carousel_right() {
+	if (carouselCounter == 3) {
+		carouselCounter = 0
+	} else {
+		++carouselCounter
+	}
+	document.getElementById("carousel").style.backgroundImage = 'url("../img/' + carouselImgs[carouselCounter] + '.jpg")'
+	// carousel.style.backgroundColor = 'red'
+}
